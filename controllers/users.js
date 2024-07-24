ab5cf854bd7e00584310ef9ee5a03f2cc6b9829f
Ram-Jam5 <edgeillusive@gmail.com>
2024-07-24 10:48:18 -0500

    create sign in route for users

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 
 const SALT_LENGTH = 12;
@@ -16,12 +17,33 @@ router.post('/signup', async (req, res) => {
         const user = await User.create({
             username: req.body.username,
             hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
-        })
-        res.status(201).json({ user })
+        });
+        const token = jwt.sign(
+            { username: user.username, _id: user._id },
+            process.env.JWT_SECRET
+        );
+        res.status(201).json({ user, token })
     } catch (error) {
       res.status(400).json({ error: error.messgae })  
     }
     res.json({ message: 'Signup route' });
 });
+
+router.post('/signin', async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+        const token = jwt.sign(
+            { username: user.username, _id: user._id },
+            process.env.JWT_SECRET
+        );
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ error: 'Invalid username or password.' });
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
 
 module.exports = router;
